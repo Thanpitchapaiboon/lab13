0e6310fbd3ad44378fc559dc8928b57cb23cb90a
Thanpitchapaiboon <thanpitchapaiboon@gmail.com>
2024-12-31 18:31:54 +0700

    131

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,49 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double x[],int n,double y[]){
+
+    
+    double sum1=0;
+    for(int c=0;c<n;c++){
+        sum1 += x[c];  
+    }
+    y[0] = sum1/n;
+
+   
+    double sum2=0;
+   for(int c=0;c<n;c++){
+        sum2 += pow((x[c]-y[0]),2);   
+    }
+    y[1]=sqrt(sum2/n);
+
+    
+    double sum3 =1;
+    for(int c=0;c<n;c++){
+    sum3 *=x[c];
+    }
+    y[2]=pow(sum3,(1.0/n));
+
+    
+    double sum4 =0;
+    for(int c=0;c<n;c++){
+        sum4 += (1/x[c]);
+    }
+    y[3]=n/sum4;
+
+    
+    y[4]=x[0];
+    for(int c=0;c<n;c++){
+        if(y[4]<x[c]){
+            y[4]=x[c];
+        }
+    }
+   
+    y[5]=x[0];
+    for(int c=0;c<n;c++){
+        if(y[5]>x[c]){
+            y[5]=x[c];
+        }
+    }
+
+}
